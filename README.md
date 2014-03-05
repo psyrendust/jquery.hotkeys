2ad674c1db6ecf9c3e4c3c8bd6c095bf9801219f
Larry Gordon <larry.gordon@xero.com>
2014-03-05 14:47:22 -0800

    Bumps version number and adds replacements for "A-Z" and "a-Z"

--- README.md
+++ README.md
@@ -72,6 +72,8 @@ The Event object that gets return to the handler has been augmented to include a
 
 ### Expansion sets that make up all keyboard keys when combined together.
 * `a-z`: a b c d e f g h i j k l m n o p q r s t u v w x y z
+* `A-Z`: A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+* `a-Z`: a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
 * `shifted`: ~ ! @ # $ % ^ & * ( ) _ + { } | : " < > ? shift+~ shift+! shift+@ shift+# shift+$ shift+% shift+^ shift+& shift+* shift+( shift+) shift+_ shift++ shift+{ shift+} shift+| shift+: shift+" shift+< shift+> shift+?
 * `nonshifted`: ` 1 2 3 4 5 6 7 8 9 0 - = [ ] \\ ; \' , . /
 * `arrow`: left up right down
--- src/jquery.hotkeys.js
+++ src/jquery.hotkeys.js
@@ -2,11 +2,13 @@
   'use strict';
 
   jQuery.hotkeys = {
-    version: '0.2.3',
+    version: '0.2.5',
 
     replacements: {
       /* sets that make up all keyboard keys when combined together */
       'a-z': 'a b c d e f g h i j k l m n o p q r s t u v w x y z',
+      'A-Z': 'A B C D E F G H I J K L M N O P Q R S T U V W X Y Z',
+      'a-Z': 'a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z',
       'shifted': '~ ! @ # $ % ^ & * ( ) _ + { } | : " < > ? shift+~ shift+! shift+@ shift+# shift+$ shift+% shift+^ shift+& shift+* shift+( shift+) shift+_ shift++ shift+{ shift+} shift+| shift+: shift+" shift+< shift+> shift+?',
       'nonshifted': '` 1 2 3 4 5 6 7 8 9 0 - = [ ] \\ ; \' , . /',
       'arrow': 'left up right down',
